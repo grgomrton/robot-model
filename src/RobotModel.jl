4e659a34b9eff66fc70649fc6aaec57e54427af1
marc <grgo.mrton@gmail.com>
2018-03-30 23:23:48 +0200

    extend tests
    concise names

--- src/RobotModel.jl
+++ src/RobotModel.jl
@@ -1,24 +1,25 @@
-# differential drive model
+# differential drive
+
 L = 0.13    # m : distance between the wheels
 r = 0.033   # m : wheel radius
 
-function initial_state(x_world, y_world, theta_world, speed_left_wheel, speed_right_wheel)
-    return [x_world y_world theta_world speed_left_wheel speed_right_wheel]
+function initial_state(x_world, y_world, theta_world, v_left_wheel, v_right_wheel)
+    return [x_world y_world theta_world v_left_wheel v_right_wheel]
 end
 
-function velocities(robot_state, control)
-    x, y, theta, speed_left_wheel, speed_right_wheel = robot_state
-    dx = r/2 * (speed_left_wheel + speed_right_wheel) * cos(theta)
-    dy = r/2 * (speed_left_wheel + speed_right_wheel) * sin(theta)
-    dtheta = r/L * (speed_right_wheel - speed_left_wheel)
+function velocities(current_state, control)
+    x, y, theta, v_left_wheel, v_right_wheel = current_state
+    dx = r/2 * (v_left_wheel + v_right_wheel) * cos(theta)
+    dy = r/2 * (v_left_wheel + v_right_wheel) * sin(theta)
+    dtheta = r/L * (v_right_wheel - v_left_wheel)
 
-    acceleration_left_wheel, acceleration_right_wheel = control
-    dspeed_left = acceleration_left_wheel
-    dspeed_right = acceleration_right_wheel
+    a_left_wheel, a_right_wheel = control
+    dv_left = a_left_wheel
+    dv_right = a_right_wheel
 
-    return [dx dy dtheta dspeed_left dspeed_right]
+    return [dx dy dtheta dv_left dv_right]
 end
 
-function state_update(robot_state, control, elapsed_time)
-    return robot_state + elapsed_time * velocities(robot_state, control)
+function state_update(state, control, elapsed_time)
+    return state + elapsed_time * velocities(state, control)
 end
--- test/RobotModel.jl
+++ test/RobotModel.jl
@@ -3,19 +3,23 @@ include("../src/RobotModel.jl")
 
 let state = initial_state(0, 0, 0, 0, 0)
     cycle_time = 0.05
+
     x, y = view(state, 1:2)
+    theta = view(state, 3)
+    v_left, v_right = view(state, 4:5)
     @test [x, y] ≈ [0, 0]
+    @test theta ≈ [0]
+    @test [v_left, v_right] ≈ [0, 0]
 
     control = [1 1]
     state = state_update(state, control, cycle_time)
-    # after first cycle the robot should not move, but acceleration should be considered
     @test view(state, 1:2) ≈ [0, 0]
-    speed_left, speed_right = view(state, 4:5)
-    @test [speed_left, speed_right] ≈ [0.05, 0.05]
+    @test view(state, 3) ≈ [0]
+    @test view(state, 4:5) ≈ [0.05, 0.05]
 
     state = state_update(state, control, cycle_time)
-    # after second cycle the robot should have moved
     @test view(state, 1:2) ≠ [0, 0]
+    @test view(state, 3) ≈ [0]
     @test view(state, 4:5) ≈ [0.1, 0.1]
 
     print("success")
